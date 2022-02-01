910a01721aab000074230b38dfb6a625e543d199
Prabesh <prabesh.bista369@gmail.com>
2022-02-01 17:25:50 +0545

    .

--- nepseapp/lib/main.dart
+++ nepseapp/lib/main.dart
@@ -9,8 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return MaterialApp(
+      home:
+    )
   }
 }
