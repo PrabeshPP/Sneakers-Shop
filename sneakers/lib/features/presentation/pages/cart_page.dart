081430e56fc2046d0546c69edb011b56c59ba8f1
Prabesh <prabesh.bista369@gmail.com>
2022-02-01 19:53:48 +0545

    .

--- sneakers/lib/features/presentation/pages/cart_page.dart
+++ sneakers/lib/features/presentation/pages/cart_page.dart
@@ -5,6 +5,7 @@ class CartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // ignore: sized_box_for_whitespace
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
