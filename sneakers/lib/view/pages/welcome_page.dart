f12c023f1812b1fe3f3cfe5c1540c0ae91da0086
Prabesh <prabesh.bista369@gmail.com>
2022-02-17 10:28:02 +0545

    designing Welcome Page

--- sneakers/lib/view/pages/welcome_page.dart
+++ sneakers/lib/view/pages/welcome_page.dart
@@ -5,13 +5,24 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Theme.of(context).canvasColor
+    return Material(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Theme.of(context).canvasColor
+        ),
+    
+        child: Column(
+          children:[
+            Image.asset("assets/welcome_shoes.png",
+          height: MediaQuery.of(context).size.height*0.4,
+          width: MediaQuery.of(context).size.width*0.7,),
+    
+          const Text("Your Favourite \n Shoes")
+          ]),
+        
       ),
-      
     );
   }
 }
