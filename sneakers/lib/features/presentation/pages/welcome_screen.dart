3fe26751d9624bd8700bb8d0b51840cdc1031f55
Prabesh <prabesh.bista369@gmail.com>
2022-02-17 10:02:39 +0545

    designing Welcome Page

--- sneakers/lib/features/presentation/pages/welcome_screen.dart
+++ sneakers/lib/features/presentation/pages/welcome_screen.dart
@@ -6,6 +6,11 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Theme.of(context).canvasColor
+      ),
       
     );
   }
