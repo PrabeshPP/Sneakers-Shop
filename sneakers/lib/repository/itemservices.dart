46b66621468d5e59a5c1a0d6e1fb04b7ca41b836
Prabesh <prabesh.bista369@gmail.com>
2022-02-21 10:18:50 +0545

    Added ItemServices

--- sneakers/lib/repository/itemservices.dart
+++ sneakers/lib/repository/itemservices.dart
@@ -1,3 +1,16 @@
-class ItemServices{
-  
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:nepseapp/model/item.dart';
+
+class ItemServices {
+  Future<List<Item>> getItem() async {
+    final jsonFile = await rootBundle.loadString("assets/JSON/file.json");
+    final jsonDecodeFile = jsonDecode(jsonFile);
+    var productsData = jsonDecodeFile["products"];
+  List<Item> itemList=List.from(productsData).map((e) =>Item.fromJson(e) ).toList();
+    
+
+    return itemList ;
+  }
 }
