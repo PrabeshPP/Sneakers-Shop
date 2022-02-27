5f950e42147055de31b542673c3f6969779584c6
Prabesh <prabesh.bista369@gmail.com>
2022-02-27 16:12:12 +0545

    Login page Design --->>>

--- sneakers/lib/view/widgets/UI/login_field.dart
+++ sneakers/lib/view/widgets/UI/login_field.dart
@@ -1,34 +1,32 @@
 import 'package:flutter/material.dart';
 
 class LoginField extends StatelessWidget {
-  const LoginField({ Key? key }) : super(key: key);
+  const LoginField({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        color: Colors.red,
-        child: Container(
-          height: 20,
-          width: MediaQuery.of(context).size.width*0.6,
-          color: Colors.yellow,
-          child:const  Padding(
-            padding:  EdgeInsets.all(32.0),
-            child:  TextField(
-              
-              decoration: InputDecoration(
-               border: OutlineInputBorder(
-                 borderRadius: BorderRadius.all(Radius.circular(32.0))
-               ),
-                hintText: "PrabeshBista"
-              ),
+      child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Center(
+            child: Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(26.0),
+                    color: Colors.red,
+                  ),
+                ),
+                Positioned(left: MediaQuery.of(context).size.width*0.1,
+                top: MediaQuery.of(context).size.height*0.014,
+                child: Icon(Icons.email)),
+               
+              ],
             ),
-          ),
-        ),
-        
-      ),
+          )),
     );
   }
 }
