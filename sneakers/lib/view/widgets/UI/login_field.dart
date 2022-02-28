2a8a5880d1d15236d560cd57a53f0208a4b9739e
Prabesh <prabesh.bista369@gmail.com>
2022-02-28 14:30:27 +0545

    Logi UI Completed

--- sneakers/lib/view/widgets/UI/login_field.dart
+++ sneakers/lib/view/widgets/UI/login_field.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 // need to complete the text form field
@@ -45,7 +46,38 @@ class _LoginFieldState extends State<LoginField> {
               ],
             ),
             //! For the Password
-            
+            Padding(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.02),
+              child: Stack(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(
+                              left: MediaQuery.of(context).size.width * 0.1),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(32.0),
+                              borderSide:
+                                  const BorderSide(width: 2, color: Colors.teal)),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(26.0),
+                              borderSide: const BorderSide(
+                                  width: 2, color: Colors.black))),
+                    ),
+                  ),
+                  Positioned(
+                    left: MediaQuery.of(context).size.width * 0.015,
+                    top: MediaQuery.of(context).size.height * 0.015,
+                    child: const Icon(
+                      CupertinoIcons.keyboard,
+                      color: Colors.black,
+                    ),
+                  )
+                ],
+              ),
+            ),
+
           ]),
     );
   }
