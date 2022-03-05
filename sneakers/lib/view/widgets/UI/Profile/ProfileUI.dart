f1c40e231c9eaac0c082445ad18baa508a0e0c47
Prabesh <prabesh.bista369@gmail.com>
2022-03-05 19:47:55 +0545

    Designed Profile UI

--- sneakers/lib/view/widgets/UI/Profile/ProfileUI.dart
+++ sneakers/lib/view/widgets/UI/Profile/ProfileUI.dart
@@ -4,15 +4,18 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:nepseapp/view/widgets/UI/Profile/UI1.dart';
 
 class ProfileUI extends StatelessWidget {
-  const ProfileUI({ Key? key }) : super(key: key);
+  const ProfileUI({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Material(
-        child: Stack(
+    return Stack(
       children: [
-        const CurvedContainer(),
+        SizedBox(
+          height: size.height,
+          width: size.width,
+        ),
+        const Positioned(child: CurvedContainer()),
         Positioned(
             top: size.height * 0.02,
             left: size.width * 0.06,
@@ -35,21 +38,38 @@ class ProfileUI extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   fontSize: 12),
             )),
-            Positioned(
+        Positioned(
             top: size.height * 0.02,
             right: size.width * 0.06,
-            child:Material(
-              borderRadius: BorderRadius.circular(20.0),
-              color: Colors.transparent,
-              child: IconButton(
+            child: Material(
+                color: Colors.transparent,
+                child: IconButton(
+                    iconSize: 30,
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.edit_sharp,
+                      color: Colors.white,
+                    )))),
+        Positioned(
+            top: size.height * 0.1,
+            right: size.width * 0.1,
+            child: Container(
+              height: size.height * 0.2,
+              width: size.width * 0.3,
+              decoration:  BoxDecoration(
+                shape: BoxShape.circle,
+                image: const DecorationImage(image: AssetImage("assets/images/goblin.jpg",
+                  ),
+                  fit: BoxFit.fill,
+                ),
+                border: Border.all(
+                  color:const Color.fromARGB(255, 138, 122, 121),
+                  width: 3
+                ),
+                ),
                 
-                onPressed: (){
-
-              }, icon: const Icon(Icons.edit_sharp,
-                color: Colors.white,))
-            )
-            ),
+            )),
       ],
-    ));
+    );
   }
 }
---
+++ sneakers/lib/view/widgets/UI/Profile/profile_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProfileBody extends StatelessWidget {
+  const ProfileBody({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
