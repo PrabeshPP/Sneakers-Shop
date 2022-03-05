6eb7ecfc771fef83b6fa8b6e4f21e81dadeb8013
Prabesh <prabesh.bista369@gmail.com>
2022-03-05 15:44:52 +0545

    impl. tranformation widget

--- sneakers/lib/view/pages/Home/profile_page.dart
+++ sneakers/lib/view/pages/Home/profile_page.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as Math;
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var size=MediaQuery.of(context).size;
+    var size = MediaQuery.of(context).size;
     return Material(
-      child: Container(
-        height: size.height,
-        width: size.width,
-      ),
-    );
+        child: Column(
+      children: [
+        Transform(
+          transform: Matrix4.rotationY(Math.pi/6),
+          child: Container(
+            height: size.height * 0.3,
+            width: size.width,
+            decoration: const BoxDecoration(color: Colors.red),
+          ),
+        ),
+      ],
+    ));
   }
 }
