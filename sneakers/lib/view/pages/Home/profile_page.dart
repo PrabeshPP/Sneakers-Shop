57b288b0076c9ba6fde25ef8d24c38dd00194f66
Prabesh <prabesh.bista369@gmail.com>
2022-03-05 17:55:28 +0545

    Profile Page

--- sneakers/lib/view/pages/Home/profile_page.dart
+++ sneakers/lib/view/pages/Home/profile_page.dart
@@ -10,15 +10,39 @@ class ProfilePage extends StatelessWidget {
     return Material(
         child: Column(
       children: [
-        Transform(
-          transform: Matrix4.rotationY(Math.pi/6),
+        ClipPath(
           child: Container(
             height: size.height * 0.3,
             width: size.width,
-            decoration: const BoxDecoration(color: Colors.red),
+            decoration: const BoxDecoration(
+                color: Colors.red,
+                borderRadius:
+                    BorderRadius.only(bottomLeft: Radius.circular(32.0))),
           ),
-        ),
+          clipper: MyClipper(),
+        )
       ],
     ));
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height * 0.8);
+    Offset firstContorlPoint = Offset(size.width * 0.1, size.height * 0.9);
+    Offset firstEndPoint = Offset(size.width * 0.15, size.height * 0.85);
+    path.quadraticBezierTo(firstContorlPoint.dx, firstContorlPoint.dy,
+        firstEndPoint.dx, firstEndPoint.dy);
+    path.lineTo(size.width, size.height * 0.4);
+    path.lineTo(size.width, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) {
+    return false;
   }
 }
