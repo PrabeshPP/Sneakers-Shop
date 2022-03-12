cf0ee954bd8da6aa9cb0cb18451444eaf3161d7a
Prabesh <prabesh.bista369@gmail.com>
2022-03-12 10:13:26 +0545

    HomePage UI

--- sneakers/lib/view/widgets/UI/homepage/home_page_ui.dart
+++ sneakers/lib/view/widgets/UI/homepage/home_page_ui.dart
@@ -11,7 +11,16 @@ class HomePageUI extends StatelessWidget {
         height:size.height,
         width: size.width,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Container(
+              height: size.height*0.06,
+              width: size.width*0.8,
+              decoration:  BoxDecoration(
+                borderRadius: BorderRadius.circular(32.0),
+                color: Colors.yellow
+              ),
+            )
             
           ],
         ),
