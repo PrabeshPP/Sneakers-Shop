dcd209854294495a5360f07d83c7b972ebd0aae9
Prabesh <prabesh.bista369@gmail.com>
2022-03-15 16:39:45 +0545

    Added Bloc for the Icons

--- sneakers/lib/repository/icons_services.dart
+++ sneakers/lib/repository/icons_services.dart
@@ -7,7 +7,7 @@ class IconsServices {
   Future<List<BrandsIcon>> getItem() async {
     final jsonFile = await rootBundle.loadString("assets/JSON/Brand.json");
     final jsonDecodeFile = jsonDecode(jsonFile);
-    var productsData = jsonDecodeFile["products"];
+    var productsData = jsonDecodeFile["Brands"];
     //mapping the data
   List<BrandsIcon> brandsIconList=List.from(productsData).map((e) =>BrandsIcon.fromJson(e) ).toList();
     
--- sneakers/lib/view/widgets/UI/homepage/brands_Icons.dart
+++ sneakers/lib/view/widgets/UI/homepage/brands_Icons.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nepseapp/model/brandsIcons.dart';
+import 'package:nepseapp/view_model/home_Item/Brands/bloc/home_bloc.dart';
 
 class BrandsIconCard extends StatelessWidget {
   const BrandsIconCard({Key? key}) : super(key: key);
@@ -8,24 +11,39 @@ class BrandsIconCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return SizedBox(
-      height: size.height * 0.06,
-      width: size.width,
-      child: ListView.builder(
-          itemCount: 10,
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.only(left: size.width * 0.03),
-              child: Container(
-                width: size.width * 0.35,
-                decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(size.width * 0.1)),
-                child: Center(child: Text("HEllo")),
-              ),
-            );
-          }),
+    return BlocBuilder<HomeBloc, HomeState>(
+      builder: (context, state) {
+        if (state is HomeInitial) {
+          return const CircularProgressIndicator();
+        } else if (state is ItemLoadedState) {
+          var listItem = state.listItem;
+          return SizedBox(
+            height: size.height * 0.06,
+            width: size.width,
+            child: ListView.builder(
+              itemCount: listItem.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.only(left: size.width * 0.03),
+                  child: Container(
+                    width: size.width * 0.35,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(size.width * 0.1)),
+                    child: Image.asset(
+                      listItem[index].icon,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                );
+              },
+            ),
+          );
+        } else {
+          return Text("Something Went Wrong!");
+        }
+      },
     );
   }
 }
