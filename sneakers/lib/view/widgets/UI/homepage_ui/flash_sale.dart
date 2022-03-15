924336577caee2b125670267c42b5c23b4c04d31
Prabesh <prabesh.bista369@gmail.com>
2022-03-15 19:29:51 +0545

    Flash Sale Component completed

--- sneakers/lib/view/widgets/UI/homepage_ui/flash_sale.dart
+++ sneakers/lib/view/widgets/UI/homepage_ui/flash_sale.dart
@@ -11,8 +11,9 @@ class FlashSale extends StatelessWidget {
       padding: EdgeInsets.only(
           top: size.height * 0.04,
           left: size.width * 0.032,
-          right: size.width * 0.032),
+          right: size.width * 0.042),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Text(
             "Flash Sale",
@@ -21,6 +22,7 @@ class FlashSale extends StatelessWidget {
                 fontFamily: GoogleFonts.roboto().fontFamily,
                 fontSize: size.height * 0.028),
           ),
+          const TimerSale()
         ],
       ),
     );
@@ -40,9 +42,37 @@ class TimerSale extends StatelessWidget {
           style: TextStyle(
               color: Colors.black,
               fontFamily: GoogleFonts.roboto().fontFamily,
-              fontSize: size.height * 0.028),
-        )
+              fontSize: size.height * 0.02),
+        ),
+        const TimeContainer(text: "01"),
+        const TimeContainer(text: "20"),
+        const TimeContainer(text: "50",)
       ],
+    );
+  }
+}
+
+class TimeContainer extends StatelessWidget {
+  final String text;
+  const TimeContainer({Key? key,required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Padding(
+      padding: EdgeInsets.only(left: size.width * 0.02),
+      child: Container(
+        height: size.height * 0.04,
+        width: size.width * 0.08,
+        color: const Color.fromARGB(255, 135, 138, 1),
+        
+        child: Center(child: Text(text,
+        style: TextStyle(
+           fontFamily: GoogleFonts.robotoMono().fontFamily,
+           color: Colors.white,
+           fontWeight: FontWeight.bold
+        ),)),
+      ),
     );
   }
 }
